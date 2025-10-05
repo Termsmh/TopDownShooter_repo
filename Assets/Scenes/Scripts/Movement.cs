449b52f63ca3fe34cf19e1c18659405c7b840f6c
Termsmh <marco.malik17@gmail.com>
2025-10-05 15:16:32 +0200

    added placeholder level, player, implemented movement

---
+++ Assets/Scenes/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector2 moveDirection;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    void Move()
+    {
+        rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY);
+    }
+}
